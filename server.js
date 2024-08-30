0706d736ac514c0da8d1ae7ea4559c4bb7d0c15e
mariogamesalt <jacobp50040@gmail.com>
2024-08-29 18:42:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 0.0.0.0;
 
 app.use(express.static('public')); // Serve static files from the 'public' directory
 app.use(express.json()); // To parse JSON bodies
